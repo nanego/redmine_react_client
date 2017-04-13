5120ab41b5cb81f534b89f2ca7362ec3d489b369
Vincent Robert <vincent.robert@nanego.com>
2017-04-13 16:33:28 +0200

    Close popup when select custom query

--- src/header/custom_queries.js
+++ src/header/custom_queries.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react'
 import { Item, Button, Image, List, Menu, Popup, Dropdown } from 'semantic-ui-react'
+import moment from 'moment'
+
+var closePopup = function(){
+  // Dirty hack to close popup
+  document.getElementsByClassName('filters_module')[0].click();
+};
 
 class CustomQueries extends Component {
 
@@ -17,12 +23,16 @@ class CustomQueries extends Component {
     switch(event.currentTarget.textContent){
       case "Mes demandes par priorité":
         this.props.replaceSelectedFilters({assigned_to:'me', order:"priority"}, true);
+        closePopup();
         break;
       case "Demandes surveillées":
         this.props.replaceSelectedFilters({is:'starred'}, true);
+        closePopup();
         break;
       case "Traité sans activité récente":
-        this.props.replaceSelectedFilters({status:'Done', updated_on:'>60.days.ago'}, true);
+        var date = moment().subtract(60, 'days').format("DD/MM/YYYY");
+        this.props.replaceSelectedFilters({status:'Done', updated_before:date}, true);
+        closePopup();
         break;
       default:
     }

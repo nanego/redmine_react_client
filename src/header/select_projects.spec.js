80105c7c08f364f92f2dce2ae656956978a1e1b7
Vincent Robert <vincent.robert@nanego.com>
2017-04-24 17:04:56 +0200

    Test a different test with Enzyme

--- src/header/select_projects.spec.js
+++ src/header/select_projects.spec.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {shallow} from 'enzyme';
 import SelectProjects from './select_projects';
 import renderer from 'react-test-renderer';
 
+var updateSelectedFilters = function(){};
+
 it('loads SelectProjects', () => {
-  var updateSelectedFilters = function(){};
   const component = renderer.create(
       <SelectProjects selected_filters={{}} updateSelectedFilters={updateSelectedFilters} />
   );
@@ -11,7 +13,7 @@ it('loads SelectProjects', () => {
   expect(select).toMatchSnapshot();
 
   // manually trigger the callback
-  // select.handleProjectsSelection();
+  // select.props.handleProjectsSelection();
   // re-rendering
   // tree = component.toJSON();
   // expect(tree).toMatchSnapshot();
@@ -21,4 +23,14 @@ it('loads SelectProjects', () => {
   // re-rendering
   // tree = component.toJSON();
   // expect(tree).toMatchSnapshot();
+});
+
+it('render component with Enzyme', () => {
+  const select = shallow(
+      <SelectProjects selected_filters={{}} updateSelectedFilters={updateSelectedFilters} />
+  );
+  expect(select.find('input').value).toEqual(undefined);
+
+  // select.find('input').simulate('change', {target { value : 'hello'}});
+  // select.find('select').simulate('change');
 });
